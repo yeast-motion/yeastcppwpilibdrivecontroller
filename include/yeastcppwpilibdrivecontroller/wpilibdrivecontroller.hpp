3eaa2f2aa0b69cae0de3b279a6d5a4d8280b8528
Michael Todd <eltodd@gmail.com>
2024-12-07 12:39:54 -0500

    Cont

--- include/yeastcppwpilibdrivecontroller/wpilibdrivecontroller.hpp
+++ include/yeastcppwpilibdrivecontroller/wpilibdrivecontroller.hpp
@@ -1,13 +1,22 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
 
 #include <yeastcpp.hpp>
+
+#include <wpimath/frc/kinematics/SwerveDriveKinematics.h>
 
 namespace yeast_motion
 {
     class WPILibDriveController : DriveController
     {
-        MotionState drive(MotionCommand command);
+        public:
+            MotionState drive(MotionCommand command);
+            WPILibDriveController(nlohmann::json characterization);
+
+        private:
+            std::vector<SwerveModule> modules;
+            std::unique_ptr<frc::SwerveDriveKinematics<4>> kinematics;
     };
 }
--- src/wpilibdrivecontroller.cpp
+++ src/wpilibdrivecontroller.cpp
@@ -1,6 +1,53 @@
+#include <exception>
+
 #include "wpilibdrivecontroller.hpp"
 
-yeast_motion::MotionState yeast_motion::WPILibDriveController::drive(yeast_motion::MotionCommand command)
+#include "wpimath/frc/estimator/SwerveDrivePoseEstimator.h"
+#include "wpimath/frc/kinematics/SwerveDriveKinematics.h"
+#include "wpimath/frc/kinematics/SwerveDriveOdometry.h"
+#include "wpimath/frc/kinematics/SwerveModulePosition.h"
+#include "wpimath/frc/kinematics/SwerveModuleState.h"
+#include "wpimath/frc/kinematics/ChassisSpeeds.h"
+#include "wpimath/frc/geometry/Rotation2d.h"
+#include "wpimath/frc/geometry/Translation2d.h"
+#include "wpimath/frc/geometry/Pose2d.h"
+
+using namespace yeast_motion;
+
+
+WPILibDriveController::WPILibDriveController(nlohmann::json characterization)
 {
+    // Need to load characterizations, incl swerve modules
+
+    if (modules.size() != 4)
+    {
+        throw std::runtime_error("Only configurations with 4 swerve modules are supported by the Yeast WPI Lib Drive Controller");
+    }
+
+    frc::Translation2d m_frontLeftLocation  {units::meter_t(modules[0].translation.x), units::meter_t(modules[0].translation.y)};
+    frc::Translation2d m_frontRightLocation {units::meter_t(modules[1].translation.x), units::meter_t(modules[1].translation.y)};
+    frc::Translation2d m_backLeftLocation   {units::meter_t(modules[2].translation.x), units::meter_t(modules[2].translation.y)};
+    frc::Translation2d m_backRightLocation  {units::meter_t(modules[3].translation.x), units::meter_t(modules[3].translation.y)};
+
+    kinematics.reset(new frc::SwerveDriveKinematics<4> 
+        (m_frontLeftLocation, 
+        m_frontRightLocation, 
+        m_backLeftLocation,
+        m_backRightLocation));
+}
+
+MotionState WPILibDriveController::drive(MotionCommand command)
+{
+    MotionState result;
+
+    frc::ChassisSpeeds speeds
+        (units::meters_per_second_t(command.velocity.x), 
+         units::meters_per_second_t(command.velocity.y), 
+         units::radians_per_second_t(command.velocity.omega));
+         
+    auto [fl, fr, bl, br] = kinematics->ToSwerveModuleStates(speeds);
+
+    auto flOptimized = frc::SwerveModuleState::Optimize(fl, units::radian_t(m_turningEncoder.GetDistance()));
+
     
 }
